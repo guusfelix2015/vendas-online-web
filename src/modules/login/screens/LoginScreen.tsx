560ba1e923079735dc275fba4c6601d21450f73f
guusfelix2015 <gusfelix2015@gmail.com>
2023-07-31 23:09:27 -0300

    feat(web): test

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,7 @@
+import { BackgroundImage } from '../styles/LoginScreen.styles';
+
+const LoginScreen = () => {
+  return <BackgroundImage src="./background.png"></BackgroundImage>;
+};
+
+export default LoginScreen;
---
+++ src/modules/login/styles/LoginScreen.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const BackgroundImage = styled.img`
+  left: 0;
+  top: 0;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+`;
